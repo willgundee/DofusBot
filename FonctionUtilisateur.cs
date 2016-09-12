91b7b9ef9c1357cf0ea4644136a7e06a398ce2fd
willgundee <cedric.hebert97@hotmail.fr>
2016-09-12 17:56:25 -0400

    non fonctionelle

--- FonctionUtilisateur.cs
+++ FonctionUtilisateur.cs
@@ -1,20 +1,9 @@
-﻿namespace GofusSharp
+﻿namespace test
 {
-    //csc /target:library /out:GofusSharp.dll FonctionUtilisateur.cs
-    public class Personage
-    {
-        int PV;
-        int PA;
-
-        public Personage(int pv, int pa)
+    class FonctionUtilisateur
+    {// help
+        public static void test(string test)
         {
-            PV = pv;
-            PA = pa;
-        }
-
-        public static int addition(int numero1, int numero2)
-        {
-            return numero1 + numero2;
+            MessageBox.Show("Test");
         }
     }
-}//Personage pTest = new Personage(5,6);MessageBox.Show(Personage.addition(pTest.PV,pTest.PA).ToString());
