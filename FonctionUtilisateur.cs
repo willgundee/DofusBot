f3be3888adfa09804f2ea69b327444e72212bd1b
willgundee <cedric.hebert97@hotmail.fr>
2016-09-12 18:00:27 -0400

    yeah

--- FonctionUtilisateur.cs
+++ FonctionUtilisateur.cs
@@ -1,9 +1,20 @@
-﻿namespace test
+﻿namespace GofusSharp
 {
-    class FonctionUtilisateur
-    {// help
-        public static void test(string test)
+    //csc /target:library /out:GofusSharp.dll FonctionUtilisateur.cs
+    public class Personage
+    {
+        int PV;
+        int PA;
+
+        public Personage(int pv, int pa)
         {
-            MessageBox.Show("Test");
+            PV = pv;
+            PA = pa;
+        }
+
+        public static int addition(int numero1, int numero2)
+        {
+            return numero1 + numero2;
         }
     }
+}//Personage pTest = new Personage(5,6);MessageBox.Show(Personage.addition(pTest.PV,pTest.PA).ToString());
