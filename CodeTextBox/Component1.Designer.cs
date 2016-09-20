7eff3cf50de6d2dc78be8081f8223ce315721891
willgundee <cedric.hebert97@hotmail.fr>
2016-09-20 19:06:59 -0400

    sync scroll better

---
+++ CodeTextBox/Component1.Designer.cs
@@ -0,0 +1,23 @@
+﻿namespace Moonlight
+{
+    partial class Component1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+        }
+
+        #endregion
+    }
+}
