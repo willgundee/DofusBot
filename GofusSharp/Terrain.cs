7264dab8c7b90a171a58ab2f75d323978c98a1e6
1422776 <1422776@LD125-16-03981>
2016-09-30 14:40:03 -0400

    fin de pathfinder A* pas tester

--- GofusSharp/Terrain.cs
+++ GofusSharp/Terrain.cs
@@ -31,9 +31,16 @@
         private class CaseAStar
         {
             public Case LaCase { get; set; }
+            public CaseAStar viensDe { get; set; }
             public int Score_g { get; set; }
             public int Score_f { get; set; }
             public CaseAStar(Case LaCase, int Score_g, int Score_f)
+            {
+                this.LaCase = LaCase;
+                this.Score_g = Score_g;
+                this.Score_f = Score_f;
+            }
+            public CaseAStar(Case LaCase)
             {
                 this.LaCase = LaCase;
                 this.Score_g = Score_g;
@@ -45,7 +52,6 @@
         {
             ListeChainee<CaseAStar> fermee = new ListeChainee<CaseAStar>();
             ListeChainee<CaseAStar> ouverte = new ListeChainee<CaseAStar>();
-            ListeChainee<Case> chemin = new ListeChainee<Case>();
             ouverte.AjouterFin(new CaseAStar(Depart, 0, DistanceEntreCases(Depart, Destination)));
 
             while (ouverte.Count != 0)
@@ -55,34 +61,42 @@
                     if (score.Score_f < courant.Score_f)
                         courant = score;
                 if (courant.LaCase == Destination)
-                    return chemin;
+                    return ReconstruireChemin(courant);
                 ouverte.Enlever(courant);
                 fermee.AjouterFin(courant);
                 foreach (Case voisin in CaseVoisines(courant.LaCase))
                 {
-                    if (fermee.Contient(voisin))
+                    foreach (CaseAStar caseAStar in fermee)
+                        if (caseAStar.LaCase == voisin)
+                            continue;
+                    int tentative_score_g = courant.Score_g + DistanceEntreCases(courant.LaCase, voisin);
+                    CaseAStar caseVoisineExistante = null;
+                    foreach (CaseAStar caseAStar in ouverte)
+                        if (caseAStar.LaCase == voisin)
+                            caseVoisineExistante = caseAStar;
+                    if (caseVoisineExistante == null)
+                    {
+                        caseVoisineExistante = new CaseAStar(voisin);
+                        ouverte.AjouterFin(caseVoisineExistante);
+                    }
+                    else if (tentative_score_g >= caseVoisineExistante.Score_g)
                         continue;
-                    int tentative_score_g = score_g.TrouverA()
+                    caseVoisineExistante.viensDe = courant;
+                    caseVoisineExistante.Score_g = tentative_score_g;
+                    caseVoisineExistante.Score_f = caseVoisineExistante.Score_g + DistanceEntreCases(caseVoisineExistante.LaCase, Destination);
                 }
             }
-
-
-            //ListeChainee<Case> chemin = new ListeChainee<Case>();
-            //ListeChainee<Case> CulDeSac = new ListeChainee<Case>();
-            //chemin.AjouterFin(Depart);
-            //while (chemin.Last.Valeur != Destination)
-            //{
-            //    if (TrouverProchaineCase(chemin, CulDeSac, Destination) == null)
-            //    {
-            //        CulDeSac.AjouterFin(chemin.Last.Valeur);
-            //        chemin.EnleverDernier();
-            //    }
-            //    else
-            //    {
-            //        chemin.AjouterFin(TrouverProchaineCase(chemin, CulDeSac, Destination));
-            //    }
-            //}
-            //return chemin;
+            return null;
+        }
+        private ListeChainee<Case> ReconstruireChemin(CaseAStar Courant)
+        {
+            ListeChainee<Case> chemin = new ListeChainee<Case>();
+            while (Courant != null)
+            {
+                chemin.AjouterDebut(Courant.LaCase);
+                Courant = Courant.viensDe;
+            }
+            return chemin;
         }
         public ListeChainee<Case> CaseVoisines(Case caseCible)
         {
