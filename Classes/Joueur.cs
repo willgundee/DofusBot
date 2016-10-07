506d0b6850be5d5411a25de172e29954bdc941a2
Lou <link_212@hotmail.com>
2016-10-07 14:14:49 -0400

    essai d'add un commit à la bd

--- Classes/Joueur.cs
+++ Classes/Joueur.cs
@@ -8,6 +8,18 @@ namespace test
 {
     public class Joueur
     {
+        public List<Equipement> Inventaire { get; set; }
+        public List<Script> LstScript { get; set; }
+        public List<Entite> LstEntite { get; set; }
+        public string NomUtilisateur { get; set; }
+        public string Courriel { get; set; }
+        public int Kamas { get; set; }
+        public int Avatar { get; set; }
+
+        public Joueur()
+        {
+
+        }
 
     }
 }
