d74abe12bdf0b74c87fc602f8e4f45fdaf9252b3
1479165 <1479165@LD125-21-03978>
2016-10-18 13:46:11 -0400

    ajout des conditions dans les équipements

--- Classes/Condition.cs
+++ Classes/Condition.cs
@@ -8,5 +8,13 @@ namespace test
 {
     public class Condition
     {
+        public Statistique Stat { get; set; }
+        public string Signe { get; set; }
+
+        public Condition(List<string> info )
+        {
+            Stat = new Statistique(info[8], Convert.ToInt32(info[6]));
+            Signe = info[2];
+        }
     }
 }
--- Classes/Joueur.cs
+++ Classes/Joueur.cs
@@ -16,8 +16,23 @@ namespace test
         public int Kamas { get; set; }
         public int Avatar { get; set; }
 
-        public Joueur(List<string> infoUser)
+        public Joueur(List<string> infoUser, List<string>[] items,List<string>[] personages,List<string>[] scripts)
         {
+            NomUtilisateur = infoUser[1];
+            Courriel = infoUser[2];
+            Kamas = Convert.ToInt32(infoUser[4]);
+            Avatar = Convert.ToInt32(infoUser[5]);
+            foreach (List<string> item in items)
+            {
+                //Inventaire.Add(new Equipement())
+            }
+            foreach (List<string> perso in personages)
+            {
+
+            }
+            foreach (List<string> script in scripts)
+                LstScript.Add( new Script(script));
+
 
         }
 
