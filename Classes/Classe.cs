7cbb444713c4f076827e3cba79286f182db841bf
Lou <link_212@hotmail.com>
2016-10-18 17:45:28 -0400

    plein de nouveau amis dans entite

--- Classes/Classe.cs
+++ Classes/Classe.cs
@@ -9,24 +9,28 @@ namespace test
    
     public class Classe
     {
-        public List<Sort> ListeSort { get; set; }
+        public List<Sort> LstSort { get; set; }
         public string Nom { get; set; }
         public string Description { get; set; }
+
         private BDService bd =new BDService();
 
         public Classe(List<string> classe)
         {
+            addSort(Convert.ToInt32(classe[0]));
             Nom = classe[1];
             Description = classe[2];
 
-            addSort(Convert.ToInt32(classe[0]));
-          /* foreach (List<string> sort in sorts)
-                ListeSort.Add(new Sort(sorts)));*/
         }
 
         private void addSort(int idClasse)
         {
-            string Sorts = "SELECT * FROM classesSorts c INNER JOIN Sorts s ON s.idSort = c.idSort Where idClasse=" + idClasse ;
+            string Sor = "SELECT * FROM Sorts s INNER JOIN classesSorts  c ON s.idSort = c.idSort Where idClasse=" + idClasse ;
+            foreach (List<string> sort in bd.selection(Sor))
+            {
+                LstSort.Add(new Sort(sort));
+            } 
+
         }
     }
 }
