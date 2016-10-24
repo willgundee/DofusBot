3f62922ea4080be3ac3cc1cd5ae2d2ed31129580
Marc-Antoine <Marc-Antoine@MARC>
2016-10-23 20:32:41 -0400

    Fix BD , Fenêtre rapport operationnel

--- FenetreRapport.xaml.cs
+++ FenetreRapport.xaml.cs
@@ -19,14 +19,54 @@ namespace test
     /// </summary>
     public partial class FenetreRapport : Window
     {
-        public FenetreRapport()
+
+        Dictionary<int, string> typeRapport;
+
+
+        BDService bdInsert;
+
+        private int idJoueur;
+
+        public FenetreRapport(int id)
         {
             InitializeComponent();
+
+            idJoueur = id;
+            bdInsert = new BDService();
+            typeRapport = new Dictionary<int, string>();
+            typeRapport.Add(1, "Rédiger une plainte");
+            typeRapport.Add(2, "Donner une suggestion");
+            typeRapport.Add(3, "Signaler un problème");
+            typeRapport.Add(4, "Autre");
+
+            cboType.ItemsSource = typeRapport;
+            cboType.SelectedIndex = 0;
         }
 
         private void btnEnvoyer_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (txtCommentaire.Text != "")
+            {
+                string message = txtCommentaire.Text;
+                string titre = txtTitre.Text;
+                int typeRapportText = cboType.SelectedIndex + 1;
+                long envoie = 0;
+
+                string inser = "INSERT INTO Rapports(idJoueur,temps,contenu,uuid,idTypeRapport,titre)VALUES(" + idJoueur + ",NOW(),'" +
+                                    message + "',UUID()," + typeRapportText + ", '" + titre + "')";
+                envoie = bdInsert.insertion(inser);
+                if (envoie == -1)
+                {
+                    System.Windows.MessageBox.Show("Une erreur s'est produite lors de l'envoie du rapport.", "Erreur");
+                }
+
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Veuillez entrer un commentaire avant d'envoyer ! ", "Erreur");
+            }
+
+
         }
     }
 }
