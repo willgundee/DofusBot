d58583c167994c98ae318e00a7ba264622f09f04
Marc-Antoine <Marc-Antoine@MARC>
2016-10-24 23:02:11 -0400

    ChoixAvatar

---
+++ choixAvatar.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace test
+{
+    /// <summary>
+    /// Interaction logic for choixAvatar.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class choixAvatar : Window
+    {
+       private ObservableCollection<BitmapImage> lstImage;
+
+        public choixAvatar(List<string> lstAvatars,int idAvatar)
+        {
+            InitializeComponent();
+           lstImage = new ObservableCollection<BitmapImage>();
+            foreach(string img in lstAvatars)
+            {
+                BitmapImage image = new BitmapImage(new Uri(img));
+
+                lstImage.Add(image);
+
+            }
+
+        }
+    }
+}
