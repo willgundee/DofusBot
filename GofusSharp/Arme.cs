a45c3234f29c82d327121bc293f2ee0b84babc83
1422776 <1422776@LD125-16-03981>
2016-11-01 12:56:49 -0400

    ligne de vue theorique (pas testé)

--- GofusSharp/Arme.cs
+++ GofusSharp/Arme.cs
@@ -7,7 +7,7 @@
         public Zone ZonePortee { get; internal set; }
         public Zone ZoneEffet { get; internal set; }
         public typeArme TypeArme { get; internal set; }
-        public int CoutPA { get; set; }
+        public int CoutPA { get; internal set; }
         internal Arme(int IdEquipement, Statistique[] TabStatistiques, string Nom, type Type, Effet[] TabEffets, Zone ZonePortee, Zone ZoneEffet, typeArme TypeArme, int CoutPA) : base(IdEquipement, TabStatistiques, Nom, Type)
         {
             this.TabEffets = TabEffets;
