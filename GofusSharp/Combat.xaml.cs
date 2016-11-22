80adddc789926b019425fecaaa4f53b077e38a6e
willgundee <cedric.hebert97@hotmail.fr>
2016-11-22 15:34:54 -0500

    ajout bouton doc

--- GofusSharp/Combat.xaml.cs
+++ GofusSharp/Combat.xaml.cs
@@ -28,6 +28,7 @@ namespace GofusSharp
         {
             InitializeComponent();
             this.Show();
+            
             CreerPartie(lstJoueurAtt, lstJoueurDef, seed);
             //System.Windows.Forms.MessageBox.Show(JsonConvert.SerializeObject(CombatCourant));
             for (int i = 0; i < 10; i++)
@@ -120,8 +121,7 @@ namespace GofusSharp
 
             mi.Invoke(null, new object[] { terrain, joueur, ListEntites });
         }
-
-        //private void Action(Terrain terrain, Entite joueur, System.Collections.ObjectModel.ReadOnlyCollection<EntiteInconnu> ListEntites)
+        
         private void Action(Terrain terrain, Entite joueur, Liste<EntiteInconnu> ListEntites)
         {
             //code dynamique 
