5f5f5e50b9561c0682d10bfaf6b22f580e6dc892
Marc <123344>
2016-11-25 15:01:05 -0500

    Legit Gestion admins + Legit Page Archive

--- Classes/Partie.cs
+++ Classes/Partie.cs
@@ -1,26 +1,33 @@
-﻿namespace Gofus
+﻿using System;
+
+namespace Gofus
 {
     public class Partie
     {
         //public int seed { get; set; }
 
-        
+
 
         public string Attaquant { get; set; }
         public string Defendant { get; set; }
 
         public string Date { get; set; }
 
+        public DateTime trueDate { get; set; }
+
         public string Gagnant { get; set; }
 
 
         private int seed { get; set; }
-        public Partie(string a , string d ,string dt,int sd,string ga)
+        public Partie(string a, string d, string dt, int sd, string ga)
         {
             Attaquant = a;
             Defendant = d;
             seed = sd;
             Date = dt;
+            trueDate = new DateTime();
+            trueDate = DateTime.ParseExact(dt, "yyyy-MM-dd HH:mm:ss,fff",System.Globalization.CultureInfo.InvariantCulture);
+           
             Gagnant = ga;
         }
 
